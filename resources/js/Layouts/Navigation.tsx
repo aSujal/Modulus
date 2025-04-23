f84c02562a11a14126b24c9cf842d58dca26bf5e
P04 <P04@DESKTOP-U5PTOGP>
2025-04-23 15:12:10 +0200

    navbar

--- resources/js/Layouts/Navigation.tsx
+++ resources/js/Layouts/Navigation.tsx
@@ -10,12 +10,14 @@ export default function Navigation({
     ...props
 }) {
     const user = usePage().props.auth.user;
+
     return (
         <aside
             id="logo-sidebar"
             className="flex flex-col items-center gap-4 bg-[#101014] pt-[9px] pb-4 w-[70px] h-full"
             aria-label="Sidebar"
         >
+   
             <div>
                 <a
                     href={route("dashboard")}
@@ -24,34 +26,28 @@ export default function Navigation({
                     <i className="mr-2 text-primary text-2xl fa-solid fa-graduation-cap"></i>
                 </a>
             </div>
+
+        
             <SidebarButton
-                icon={<Home />}
+                icon={Home} 
                 label="Home"
                 onClick={() => route("dashboard")}
                 isActive={route().current("dashboard")}
             />
-            <div>
-                <ul className="space-y-2">
-                    <li>
-                        <NavLinkWithIcon
-                            href={route("profile.edit")}
-                            active={route().current("profile.edit")}
-                            icon={<User />}
-                        >
-                            {user.full_name}
-                        </NavLinkWithIcon>
-                    </li>
-                    <li>
-                        <NavLinkWithIcon
-                            method="post"
-                            href={route("logout")}
-                            active={false}
-                            icon={<LogOut />}
-                        >
-                            Sign Out
-                        </NavLinkWithIcon>
-                    </li>
-                </ul>
+
+         
+            <div className="mt-auto w-full ">
+                <SidebarButton
+                    className="dark:bg-slate-700 bg-slate-200"
+                    onClick={() => route("profile.edit")}
+                    isActive={route().current("profile.edit")}>
+                    {user?.full_name?.charAt(0)}
+                </SidebarButton>
+                <SidebarButton
+                    icon={LogOut}
+                    onClick={() => route("logout")}
+                    isActive={false}
+                />
             </div>
         </aside>
     );
